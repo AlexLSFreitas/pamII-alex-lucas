9b7583437b4dbd656c3fccb6af1e9ad85ca6fe6e
AlexLSFreitas <alex.lucas.14.02.2006@gmail.com>
2023-03-08 08:41:24 -0300

    Aula sobre JS Fyunções e variáveis

---
+++ aula02/funcoes.js
@@ -0,0 +1,20 @@
+let num1 =2
+const num2 = 3
+
+function soma() {
+    return num1+num2
+}
+
+console.log(soma())
+
+num1 = 6
+// num2 = 9
+
+console.log(soma())
+
+const FuncaoSoma = (a,b) => {
+    return a+b
+}
+
+console.log(FuncaoSoma(10, 8))
+console.log(FuncaoSoma(10, 20))
---
+++ aula02/variaveis.js
@@ -0,0 +1,22 @@
+//tipos de variaveis
+// 
+var variavel1 = "olá"
+//
+let variavel2 = 2
+//
+const variavel3 = 3
+
+const obj = {
+    nome:"Alex",
+    signo:"CobraKai"
+    //formato JSON - JavaScript 
+}
+
+//como declarar uma variavel
+
+console.log(variavel1, "galerinha do ", variavel3+"º ano")
+console.log(variavel2 + variavel3)
+console.log(variavel2, variavel3)
+console.log(typeof variavel3)
+console.log(obj.nome,obj.signo)
+console.log(typeof obj)
